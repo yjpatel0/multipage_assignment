9d9770a098950f90481cff7c28428c189818024f
yjpatel0 <yash91200@gmail.com>
2024-02-09 22:34:34 -0500

    Initial Commit

---
+++ multipage_assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  multipage_assignment
+//
+//  Created by user237705 on 2/9/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ multipage_assignmentUITests/multipage_assignmentUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  multipage_assignmentUITestsLaunchTests.swift
+//  multipage_assignmentUITests
+//
+//  Created by user237705 on 2/9/24.
+//
+
+import XCTest
+
+final class multipage_assignmentUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
